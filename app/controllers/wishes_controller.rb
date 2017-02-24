a0a4430d0ad0f240c1b1f71d4c6e7a2af39729f0
Luise Rauch <luise.rauch@gmail.com>
2017-02-24 14:12:33 +0100

    added more CRUD

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,7 @@
 class WishesController < ApplicationController
-  # def show
-  #   @wish = Wish.find(params[:id])
-  # end
+  def show
+    @wish = Wish.find(params[:id])
+  end
 
   def new
     @wish = Wish.new
@@ -22,5 +22,32 @@ class WishesController < ApplicationController
   end
 
   def edit
+    @wish = Wish.find(params[:id])
+  end
+
+  def update
+    @wish = Wish.find(params[:id])
+    @wish.title = params[:wish][:title]
+    @wish.body = params[:wish][:body]
+
+    if @wish.save
+      flash[:notice] = "Wish was updated successfully."
+      redirect_to @wish
+    else
+      flash.now[:alert] = "There was an error saving the wish. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wish = Wish.find(params[:id])
+
+    if @wish.destroy
+      flash[:notice] = "\"#{@wish.title}\" was deleted successfully."
+      # redirect_to wishes_path
+    else
+      flash.now[:alert] = "There was an error deleting the wish."
+      render :show
+    end
   end
 end
--- spec/controllers/wishes_controller_spec.rb
+++ spec/controllers/wishes_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WishesController, type: :controller do
-
+  let(:my_wish) { Wish.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET new" do
     it "returns http success" do
@@ -36,19 +36,56 @@ RSpec.describe WishesController, type: :controller do
     end
   end
 
-  # describe "GET show" do
-  #   it "returns http success" do
-  #     get :show, {id: my_wish.id}
-  #     expect(response).to have_http_status(:success)
-  #   end
-  #   it "renders the #show view" do
-  #     get :show, {id: my_wish.id}
-  #     expect(response).to render_template :show
-  #   end
-  #
-  #   it "assigns my_wish to @wish" do
-  #     get :show, {id: my_wish.id}
-  #     expect(assigns(:wish)).to eq(my_wish)
-  #   end
-  # end
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, {id: my_wish.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, {id: my_wish.id}
+      expect(response).to render_template :edit
+    end
+
+    it "assigns wish to be updated to @wish" do
+      get :edit, {id: my_wish.id}
+
+      wish_instance = assigns(:wish)
+
+      expect(wish_instance.id).to eq my_wish.id
+      expect(wish_instance.title).to eq my_wish.title
+      expect(wish_instance.body).to eq my_wish.body
+    end
+  end
+
+  describe "PUT update" do
+    it "updates wish with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_wish.id, wish: {title: new_title, body: new_body}
+
+      updated_wish = assigns(:wish)
+      expect(updated_wish.id).to eq my_wish.id
+      expect(updated_wish.title).to eq new_title
+      expect(updated_wish.body).to eq new_body
+    end
+
+    it "redirects to the updated wish" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_wish.id, wish: {title: new_title, body: new_body}
+      expect(response).to redirect_to my_wish
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the wish" do
+      delete :destroy, {id: my_wish.id}
+      count = Wish.where({id: my_wish.id}).size
+      expect(count).to eq 0
+    end
+  end
+
 end
