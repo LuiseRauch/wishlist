8bfd780e0b1b6afb795653830af75bc44eba30d0
Luise Rauch <luise.rauch@gmail.com>
2017-02-28 14:25:58 +0100

    added wish validation and a partial

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,4 +1,8 @@
 class Wish < ApplicationRecord
   belongs_to :list
   has_many :comments, dependent: :destroy
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :list, presence: true
 end
--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe Wish, type: :model do
 
   it { is_expected.to belong_to(:list) }
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:list) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
   describe "attributes" do
     it "has title and body attributes" do
       expect(wish).to have_attributes(title: title, body: body)
